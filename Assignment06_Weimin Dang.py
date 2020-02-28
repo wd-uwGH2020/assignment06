89cca1694c5296fcb4f5bedb532677f2b82fc751
wd-uwGH2020 <61220048+wd-uwGH2020@users.noreply.github.com>
2020-02-28 15:39:37 -0800

    Add files via upload

---
+++ Assignment06_Weimin Dang.py
@@ -0,0 +1,264 @@
+#------------------------------------------#
+# Title: Assignment06_Starter.py
+# Desc: Working with classes and functions.
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+# Wdang, 2020-Feb-28, Modified File
+#------------------------------------------#
+
+# -- DATA -- #
+strChoice = '' # User input
+lstTbl = []  # list of lists to hold data
+dicRow = {}  # list of data row
+strFileName = 'CDInventory.txt'  # data storage file
+objFile = None  # file object
+
+
+# -- PROCESSING -- #
+class DataProcessor:
+    # TODOne add functions for processing here
+    """Adding or deleting data to be processed"""
+    
+    @staticmethod
+    def add_inventory(table, row):
+        """Add user inputs of new CD to current inventory table
+
+
+        Args:
+            Unpack variables of user inputs collected 
+            Convert user input CD ID to integer format
+            row (dictionary): 2D data strucure (dict) that convert user inputs to the library format
+
+        Returns:
+            table (list of dicts): new data are added to the table .
+
+        """
+        strID, strTitle, strArtist = IO.user_input_to_add_inventory()
+        intID = int(strID)
+        row = {'ID': intID, 'Title': strTitle, 'Artist': strArtist}
+        table.append(row)
+
+    @staticmethod
+    def delete_inventory(table):
+        """Delete CD inventory from current inventory table based on user input CD ID using a counter intRowNr, and confirm the deletion with user
+
+
+        Args:
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime.
+
+        Returns:
+            table (list of dict): new data are added to the table .
+
+        """
+        intRowNr = -1
+        blnCDRemoved = False
+        for row in lstTbl:
+            intRowNr += 1
+            if row['ID'] == intIDDel:
+                del table[intRowNr]
+                blnCDRemoved = True
+                break
+        if blnCDRemoved:
+            print('The CD was removed')
+        else:
+            print('Could not find this CD!')
+
+
+
+class FileProcessor:
+    """Processing the data to and from text file"""
+
+    @staticmethod
+    def read_file(file_name, table):
+        """Function to manage data ingestion from file to a list of dictionaries
+
+        Reads the data from file identified by file_name into a 2D table
+        (list of dicts) table one line in the file represents one dictionary row in table.
+
+        Args:
+            file_name (string): name of file used to read the data from
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime
+
+        Returns:
+            None.
+        """
+        table.clear()  # this clears existing data and allows to load data from file
+        objFile = open(file_name, 'r')
+        for line in objFile:
+            data = line.strip().split(',')
+            dicRow = {'ID': int(data[0]), 'Title': data[1], 'Artist': data[2]}
+            table.append(dicRow)
+        objFile.close()
+
+    @staticmethod
+    def write_file(file_name, table):
+        # TODOne Add code here
+        """Function to manage data ingestion from a list of dictionaries to a file
+
+        Overwrite the data from file identified by file_name with the follow:
+        Extract the values only of the each row of the list of dicts as a new list, one row a time
+        Convert the first value to string format
+        Write each of the values comma separated, and start a new line at the end
+        
+        Args:
+            file_name (string): name of file used to read the data from
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime
+
+        Returns:
+            None.
+        """        
+        objFile = open(file_name, 'w')
+        for row in table:
+            lstValues = list(row.values())
+            lstValues[0] = str(lstValues[0])
+            objFile.write(','.join(lstValues) + '\n')
+        objFile.close()
+
+
+# -- PRESENTATION (Input/Output) -- #
+
+class IO:
+    """Handling Input / Output"""
+
+    @staticmethod
+    def print_menu():
+        """Displays a menu of choices to the user
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+
+        print('Menu\n\n[l] load Inventory from file\n[a] Add CD\n[i] Display Current Inventory')
+        print('[d] delete CD from Inventory\n[s] Save Inventory to file\n[x] exit\n')
+
+    @staticmethod
+    def menu_choice():
+        """Gets user input for menu selection
+
+        Args:
+            None.
+
+        Returns:
+            choice (string): a lower case sting of the users input out of the choices l, a, i, d, s or x
+
+        """
+        choice = ' '
+        while choice not in ['l', 'a', 'i', 'd', 's', 'x']:
+            choice = input('Which operation would you like to perform? [l, a, i, d, s or x]: ').lower().strip()
+        print()  # Add extra space for layout
+        return choice
+
+    @staticmethod
+    def show_inventory(table):
+        """Displays current inventory table
+
+
+        Args:
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime.
+
+        Returns:
+            None.
+
+        """
+        print('======= The Current Inventory: =======')
+        print('ID\tCD Title (by: Artist)\n')
+        for row in table:
+            print('{}\t{} (by:{})'.format(*row.values()))
+        print('======================================')
+
+    # TODOne add I/O functions as needed
+
+    @staticmethod
+    def user_input_to_add_inventory():
+        """Collect user inputs to add new CDs to inventory
+
+
+        Args:
+            None.
+
+        Returns:
+            User inputs as string variables.
+
+        """
+        str1 = input('Enter ID: ').strip()
+        str2 = input('What is the CD\'s title? ').strip()
+        str3 = input('What is the Artist\'s name? ').strip()
+        return str1, str2, str3
+
+
+
+# 1. When program starts, read in the currently saved Inventory
+FileProcessor.read_file(strFileName, lstTbl)
+
+# 2. start main loop
+while True:
+    # 2.1 Display Menu to user and get choice
+    IO.print_menu()
+    strChoice = IO.menu_choice()
+
+    # 3. Process menu selection
+    # 3.1 process exit first
+    if strChoice == 'x':
+        break
+    # 3.2 process load inventory
+    if strChoice == 'l':
+        print('WARNING: If you continue, all unsaved data will be lost and the Inventory re-loaded from file.')
+        strYesNo = input('type \'yes\' to continue and reload from file. otherwise reload will be canceled')
+        if strYesNo.lower() == 'yes':
+            print('reloading...')
+            FileProcessor.read_file(strFileName, lstTbl)
+            IO.show_inventory(lstTbl)
+        else:
+            input('canceling... Inventory data NOT reloaded. Press [ENTER] to continue to the menu.')
+            IO.show_inventory(lstTbl)
+        continue  # start loop back at top.
+    # 3.3 process add a CD
+    elif strChoice == 'a':
+        # 3.3.1 Ask user for new ID, CD Title and Artist
+        # TODOne move IO code into function
+        # 3.3.1 is embedded in 3.3.2
+        
+        # 3.3.2 Add item to the table
+        # TODOne move processing code into function
+        DataProcessor.add_inventory(lstTbl, dicRow)
+        IO.show_inventory(lstTbl)
+        continue  # start loop back at top.
+    # 3.4 process display current inventory
+    elif strChoice == 'i':
+        IO.show_inventory(lstTbl)
+        continue  # start loop back at top.
+    # 3.5 process delete a CD
+    elif strChoice == 'd':
+        # 3.5.1 get Userinput for which CD to delete
+        # 3.5.1.1 display Inventory to user
+        IO.show_inventory(lstTbl)
+        # 3.5.1.2 ask user which ID to remove
+        intIDDel = int(input('Which ID would you like to delete? ').strip())
+        # 3.5.2 search thru table and delete CD
+        # TODOne move processing code into function
+        DataProcessor.delete_inventory(lstTbl)
+        IO.show_inventory(lstTbl)
+        continue  # start loop back at top.
+    # 3.6 process save inventory to file
+    elif strChoice == 's':
+        # 3.6.1 Display current inventory and ask user for confirmation to save
+        IO.show_inventory(lstTbl)
+        strYesNo = input('Save this inventory to file? [y/n] ').strip().lower()
+        # 3.6.2 Process choice
+        if strYesNo == 'y':
+            # 3.6.2.1 save data
+            # TODOne move processing code into function
+            FileProcessor.write_file(strFileName, lstTbl)
+        else:
+            input('The inventory was NOT saved to file. Press [ENTER] to return to the menu.')
+        continue  # start loop back at top.
+    # 3.7 catch-all should not be possible, as user choice gets vetted in IO, but to be safe:
+    else:
+        print('General Error')
+
+
+
+
